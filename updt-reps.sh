1eebe1508ab21171556d399ba58c130eebc07cab
Hugo Matheus <48367803+hugomatheuss@users.noreply.github.com>
2022-10-21 11:41:46 -0300

    upgrading

--- updt-reps.sh
+++ updt-reps.sh
@@ -4,6 +4,9 @@ cd /vox
 
 composerCacheDir="/vox/composer-cache"
 apacheFilesDir="/vox/apache-files"
+assetsDir="/vox/assets-sigfacil"
+sigfacilExternalFront="/vox/sigfacil-external-front"
+sigfacilDir="/vox/sigfacil"
 
 for D in /vox/*; do
     if [ -d "$D" ] && [ "$D" != $composerCacheDir ] && [ "$D" != $apacheFilesDir ]; then
@@ -11,15 +14,21 @@ for D in /vox/*; do
         currentBranch=`git rev-parse --abbrev-ref HEAD`
 
         if [ "$currentBranch" = "master" ]; then
-            echo "Atualizando o projeto $D"
+            printf "\n\n\n \e[32m Atualizando o projeto $D \n\n\n"
             git fetch upstream
             git pull upstream master
         else
-            echo "estava no branch $currentBranch"
-            echo "Atualizando o projeto $D"
+            printf "Branch atual ==> $currentBranch\n"
+            printf "Trocando para branch master\n"
+            printf "\n\n\n Atualizando o projeto $D \n\n\n"
             git checkout master
             git fetch upstream
             git pull upstream master
+        fi
+
+        if [ "$assetsDir" = $D ] || [ "$sigfacilExternalFront" = $D ] || [ "$sigfacilDir" = $D ]; then
+            printf "\n\n\n Instalando novas dependências do front-end \n\n\n"
+            npm install && npm run build
         fi
         cd ..
     fi
